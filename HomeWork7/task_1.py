d3d11a1fcf7d34fbe7858adb5330c6486dd63f8d
ArkadyF <burntobe@gmail.com>
2022-06-16 08:38:43 +0500

    hw 7

---
+++ HomeWork7/task_1.py
@@ -0,0 +1,16 @@
+from libs.exchange import Rate
+
+
+class ExtendRate(Rate):
+    def max_rate_name(self):
+        currency_dict = list(self.exchange_rates().values())
+        print(currency_dict)
+        max_val = max(currency_dict, key=lambda key: key['Value'])
+
+        return max_val['Name']
+
+
+rate = ExtendRate()
+name_max_currency = rate.max_rate_name()
+
+print(name_max_currency)
