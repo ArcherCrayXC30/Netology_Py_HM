2dc7586eea9abcc5b14ded2073a901fc9ea4aae2
ArkadyF <burntobe@gmail.com>
2022-07-12 16:10:55 +0500

    done #4

--- PYDA-34/HomeWork3/task_2.py
+++ PYDA-34/HomeWork3/task_2.py
@@ -1,3 +1,5 @@
+import time
+
 queries = [
     'смотреть сериалы онлайн',
     'новости спорта',
@@ -10,7 +12,7 @@ queries = [
 
 queries_dict = {}
 all_queries_leng = len(queries)
-
+start1 = time.time()
 for val in queries:
     word_arr = val.split(" ")
     if len(word_arr) in queries_dict:
@@ -20,3 +22,13 @@ for val in queries:
 
 for key, val in queries_dict.items():
     print(f'Поисковых запросов, содержащих {key} слов(а): {val/all_queries_leng:2.2%}')
+end1 = time.time()
+
+start2 = time.time()
+len_q = [len(query.split()) for query in queries]
+for i in sorted(set(len_q)):
+    print(f"Поисковых запросов, содержащих {i} слов(а): {len_q.count(i)/all_queries_leng:.2%}")
+end2 = time.time()
+
+print(start1, end1, '---', end1 - start1)
+print(start2, end2, '---', end2 - start2)
